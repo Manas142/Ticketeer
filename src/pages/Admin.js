0b703261aa113acaba6f4912e68b7a9d7e11a4ad
utmalik <utmalik@amazon.com>
2023-05-13 10:33:21 +0530

    user Update Modal

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import {getAllTickets, updateTicket} from "../api/ticket";
-import {getAllUsers} from "../api/user";
+import {getAllUsers, updateUser} from "../api/user";
 import MaterialTable from 'material-table'
 import {Modal, Button} from "react-bootstrap";
 
@@ -14,6 +14,9 @@ function Admin(){
     const [ticketStatusCount, setTicketStatusCount] = useState({});
     const [selectedCurrTicket, setSelectedCurrTicket] = useState({});
     const [ticketUpdateModal, setTicketUpdateModal] = useState(false);
+
+    const [usersUpdateModal, setUsersUpdateModal] = useState(false);
+    const [selectedCurrUser, setSelectedCurrUser]=useState(false);
 
     const [userDetails,setUserDetails]=useState([]);
 
@@ -28,18 +31,6 @@ function Admin(){
         .then(res=>{
            setTicketDetails(res.data);
            updateTicketsCount(res.data);
-           console.log(res.data);
-        })
-        .catch(err=>{
-            console.log(err);
-        })
-    }
-
-        const fetchUsers=()=>{
-
-        getAllUsers()
-        .then(res=>{
-           setUserDetails(res.data);
            console.log(res.data);
         })
         .catch(err=>{
@@ -114,6 +105,61 @@ function Admin(){
         .catch(err=>{
             console.log(err.message);
         })
+    }
+
+
+    const fetchUsers=()=>{
+
+        getAllUsers()
+        .then(res=>{
+           setUserDetails(res.data);
+           console.log(res.data);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+
+    const closeUsersUpdateModal=()=>{
+        setUsersUpdateModal(false);
+    }
+
+    const editUser=(userDetail)=>{
+        setSelectedCurrUser(userDetail);
+        setUsersUpdateModal(true);
+
+    }
+
+    const changeUserDetails=(e)=>{
+        console.log(e.target.value);
+
+        if(e.target.name==="status"){
+            selectedCurrUser.userStatus=e.target.value;
+        }
+
+        setSelectedCurrUser({...selectedCurrUser});
+    }
+
+    const updateUserFn=(e)=>{
+        e.preventDefault();
+        
+        const userData={
+            _id:selectedCurrUser._id,
+            status:selectedCurrUser.userStatus
+        }
+
+
+        updateUser(userData)
+        .then(res=>{
+            if(res.status===200){
+                console.log("User Updated Successfully");
+                setUsersUpdateModal(false);
+            }
+        })
+        .catch(err=>{
+            console.log(err.message);
+        })
+
     }
 
 
@@ -238,6 +284,8 @@ function Admin(){
             { title: 'STATUS', field: 'userStatus' },
           ]}
 
+          onRowClick={(event,rowData)=>editUser(rowData)}
+
           data={userDetails}
 
           title="USER RECORDS"
@@ -245,12 +293,70 @@ function Admin(){
           options={{
 
             sorting:true,
+            filtering:true,
             rowStyle:{
                 cursor:"pointer"
             }
           }}      
         />
 
+           <Modal show={usersUpdateModal} onHide={closeUsersUpdateModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Details</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+
+            <form onSubmit={updateUserFn}>
+
+                <div className="p-1">
+                    <h5 className="card-subtitle mb-2 text-primary">
+                     UserId : {selectedCurrUser.userId} 
+                      </h5>
+
+                             <h5 className="card-subtitle mb-2 text-primary">
+                     UserType : {selectedCurrUser.userTypes} 
+                      </h5>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" > Name </span>
+                        <input type="text" disabled name="user" value={selectedCurrUser.name}  />
+                    </div>
+
+                     <div className="input-group mb-3">
+                        <span className="input-group-text" > email </span>
+                        <input type="text" disabled name="email" value={selectedCurrUser.email}  />
+                    </div>
+
+                       <div className="input-group mb-3">
+                        <span className="input-group-text" > Status </span>
+
+                        <select name="status" value={selectedCurrUser.userStatus} onChange={changeUserDetails} className="form-select" >
+
+                            <option value="APPROVED"> APPROVED </option>
+                            <option value="PENDING" > PENDING </option>
+                            <option value="REJECTED" > REJECTED </option>
+
+                        </select>
+
+                    </div>
+
+                 
+
+                </div>
+
+                     <Button variant="secondary" onClick={closeUsersUpdateModal}>
+            Close
+          </Button>
+          <Button type="submit" variant="primary">
+            Update
+          </Button>
+            </form>
+
+        </Modal.Body>
+        <Modal.Footer>
+     
+        </Modal.Footer>
+      </Modal>
 
 
 
